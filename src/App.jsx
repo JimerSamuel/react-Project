0466fe00598f8935b06a57d24df3dcc18e22e22c
Jimer Samuel Espinoza <ejimersamuel@yahoo.com>
2023-06-12 22:56:00 -0500

    1.0.3

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img
+          src="https://unavatar.io/gravatar/sindresorhus@gmail.com"
+          alt="Avatar profile"
+        />
+        <div>
+          <strong>Persona Random</strong>
+          <span>@RamdonPerson</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
